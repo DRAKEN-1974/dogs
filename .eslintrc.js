49fd8d7acda75866d078b1c02dca6727f696dd3a
DRAKEN-1974 <150982428+DRAKEN-1974@users.noreply.github.com>
2025-05-04 13:12:06 +0530

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  extends: 'next/core-web-vitals',
+  rules: {
+   
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@next/next/no-img-element': 'warn',
+    'prefer-const': 'warn',
+    'react/no-unescaped-entities': 'warn'
+  }
+}
